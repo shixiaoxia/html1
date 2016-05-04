6d8ec3a6b546e8c562e2885f77d6b57d9322614a
shixiaoxia <wangshi66@live.cn>
2016-05-04 11:07:29 +0800

    作品
    作品集

---
+++ 仿佳能/scripts/jianeng.js
@@ -0,0 +1,22 @@
+/**
+ * Created by lxx on 2016/5/3.
+ */
+$(document).ready(function () {
+    $("a").hover(function () {
+        $(this).css('background-position-x','-123px');
+        
+        
+    },function () {
+        $(this).css('background-position-x','0');
+        
+        
+
+
+    })
+});
+$(function () {
+    
+})
+
+    
+
